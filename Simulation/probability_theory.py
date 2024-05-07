fc5975fc724e25e45f2e2aca2e8c85546ac73a8d
Guillaume Sintes <guillaumesintes@orange.fr>
2024-05-07 17:53:34 +0200

    Make a class structure

--- Simulation/probability_theory.py
+++ Simulation/probability_theory.py
@@ -19,57 +19,68 @@ def distrib_norm_vel(v: float) -> float:
         return 0
     return lognorm.pdf(v, 1)
 
-def cumulative_norm_vel(v: float) -> float:
-    """Give the probability that the velocity is smaller than v."""
-    return quad(distrib_norm_vel, 0, v)[0]
+class Probability_knowing_vc:
+    """Calculate the probabilities at v_c fixed."""
+    def __init__(self, distrib_norm_vel: Callable[[float], float], v_c:float) -> None:
+        assert v_c >= 0
+        self.v_c = v_c
+        self.distrib_norm_vel = distrib_norm_vel
+        self.infinity_encounter_probability()
 
+    def cumulative_norm_vel(self, v: float) -> float:
+        """Give the probability that the velocity is smaller than v."""
+        return quad(self.distrib_norm_vel, 0, v)[0]
+
+    def distrib_vel_rel_knowingSc(self, v_hat: float, s_c: int) -> float:
+        """Calculate the distribution of the relative velocity knowing Sc."""
+        assert s_c in (-1, 1)
+        if s_c == 1:
+            return distrib_norm_vel(v_hat + self.v_c) / 2 + distrib_norm_vel(-(v_hat + self.v_c)) / 2
+        else:
+            return distrib_norm_vel(self.v_c - v_hat) / 2 + distrib_norm_vel(v_hat - self.v_c) / 2
+        
+    def distrib_vel_rel(self, v_hat: float) -> float:
+        """Calculate the distribution of the relative velocity."""
+        return (self.distrib_vel_rel_knowingSc(v_hat, 1)  + self.distrib_vel_rel_knowingSc(v_hat, -1)) / 2
+
+    def vel_rel_cumulative_knowingSc(self, v_hat: float, s_c: int) -> float:
+        """Calculate the cumulative distribution of the relative velocity knowing Sc."""
+        assert s_c in (-1, 1)
+        return quad(lambda x: self.distrib_vel_rel(x), 0, v_hat)[0]
+
+    def infinity_encounter_probability(self) -> None:
+        """Probability to have an encounter at infinity."""
+        p_v_inf_v_c = self.cumulative_norm_vel(self.v_c)
+        self.pinf = (1 - p_v_inf_v_c ** 2) / 4
     
-def distrib_vel_rel_knowingSc(v_hat: float, v_c: float, s_c: int) -> float:
-    """Calculate the distribution of the relative velocity knowing Sc."""
-    assert s_c in (-1, 1)
-    if s_c == 1:
-        return distrib_norm_vel(v_hat + v_c) / 2 + distrib_norm_vel(-(v_hat + v_c)) / 2
-    else:
-        return distrib_norm_vel(v_c - v_hat) / 2 + distrib_norm_vel(v_hat - v_c) / 2
+    def cumulative_time_encounter(self, t: float) -> float:
+        """Calculate the culmulative distribution of the time of encounter."""
+
+        bigger_than = ((self.vel_rel_cumulative_knowingSc(D0 / t, 1)) ** 2  + 
+                        (self.vel_rel_cumulative_knowingSc(D0 / t, -1)) **  2) / 2 
+        return  1 - self.pinf - bigger_than
+
+    def distribution_time_encounter(self, t: float) -> float:
+        """Calculate the distribution of the tine of encounter."""
+        return derivative(lambda x: self.cumulative_time_encounter(x), t, dx=1)
     
-def distrib_vel_rel(v_hat: float, v_c: float) -> float:
-    """Calculate the distribution of the relative velocity."""
-    return (distrib_vel_rel_knowingSc(v_hat, v_c, 1)  + distrib_vel_rel_knowingSc(v_hat, v_c, -1)) / 2
+    @staticmethod
+    def esperance(distribution: Callable[[float], float], min_val: float=0, max_val: float = 100) -> float:
+        """Calculate the esperance of the distribution."""
+        return quad(lambda x: x * distribution(x), min_val, max_val)
 
-
-def vel_rel_cumulative_knowingSc(v_hat: float, v_c: float, s_c: int) -> float:
-    """Calculate the cumulative distribution of the relative velocity knowing Sc."""
-    assert s_c in (-1, 1)
-    return quad(lambda x: distrib_vel_rel(x, v_c), 0, v_hat)[0]
-
-def infinity_encounter_probability(v_c: float) -> float:
-    """Probability to have an encounter at infinity."""
-    p_v_inf_v_c = cumulative_norm_vel(v_c)
-    return (1 - p_v_inf_v_c ** 2) / 4
-   
-
-def culmulative_time_encounter(t: float, v_c: float=1) -> float:
-    """Calculate the culmulative distribution of the time of encounter."""
-
-    bigger_than = ((vel_rel_cumulative_knowingSc(D0 / t, v_c, 1)) ** 2  + 
-                    (vel_rel_cumulative_knowingSc(D0 / t, v_c, -1)) **  2) / 2 
-    pinf = infinity_encounter_probability(v_c)
-    return  1 - pinf - bigger_than
-
-def distribution_time_encouter(t: float, v_c: float) -> float:
-    """Calculate the distribution of the tine of encounter."""
-    return derivative(lambda x: culmulative_time_encounter(x, v_c), t, dx=1)
-
-def esperance(distribution: Callable[[float], float], min_val: float=0, max_val: float = 100) -> float:
-    """Calculate the esperance of the distribution."""
-    return quad(lambda x: x * distribution(x), min_val, max_val)
 
 if __name__=="__main__":
-    t = np.linspace(0., 20, 100)
-    pdf = list(map(infinity_encounter_probability, t))
+    v_cs = [0.1, 1, 2, 10]
+    t = np.linspace(0.1, 20, 200)
     plt.figure()
-    plt.plot(t, pdf)
-    plt.xlabel("$v_c$")
-    plt.ylabel("$p(T_e=+\\infty)$")
+    for v_c in v_cs:
+        proba = Probability_knowing_vc(distrib_norm_vel, v_c)       
+        pdf = list(map(proba.cumulative_time_encounter, t))
+        
+        plt.plot(t, pdf, label=v_c)
+    plt.xlabel("$T_e$")
+    plt.ylabel("$p(T_e)$")
+    plt.legend()
     plt.show(block=True)
     # print(pdf[-1] + infinity_encounter_probability(v_c=1))
